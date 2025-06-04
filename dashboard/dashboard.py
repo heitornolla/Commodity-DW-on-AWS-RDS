586bbf2c4d68ded637d2c60bb2c67d0bd09cc58c
heitor <heitor.nolla@gmail.com>
2025-06-04 14:50:43 -0300

    starting dashboard

---
+++ dashboard/dashboard.py
@@ -0,0 +1,71 @@
+import os
+
+from dotenv import load_dotenv
+import pandas as pd
+import streamlit as st
+from sqlalchemy import create_engine
+from sqlalchemy.exc import ProgrammingError
+
+load_dotenv()
+
+DB_HOST = os.getenv('DB_HOST_PROD')
+DB_PORT = os.getenv('DB_PORT_PROD')
+DB_NAME = os.getenv('DB_NAME_PROD')
+DB_USER = os.getenv('DB_USER_PROD')
+DB_PASS = os.getenv('DB_PASS_PROD')
+DB_SCHEMA = os.getenv('DB_SCHEMA_PROD')
+
+DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+engine = create_engine(DATABASE_URL)
+
+
+def get_data():
+    query = f"""
+    SELECT
+        date,
+        symbol,
+        closing_value,
+        action,
+        quantity,
+        value,
+        earnings
+    FROM
+        public.dm_commodities;
+    """
+    try:
+        df = pd.read_sql(query, engine)
+        return df
+    except ProgrammingError as e:
+        st.error("Error acessing table")
+        return pd.DataFrame()  
+
+
+st.set_page_config(page_title='Dashboard', layout='wide')
+
+
+st.title('Commodities Dashboard')
+
+
+st.write("Data from commodities and their respective transactions")
+
+df = get_data()
+
+if df.empty:
+    st.write("Unable to load data")
+else:
+    st.write("### Data")
+    st.dataframe(df)
+
+
+    st.write("### Summary")
+    st.write(df.describe())
+
+
+    st.write("### Graphs")
+
+
+    st.bar_chart(df[['date', 'earnings']].set_index('date'))
+
+
+    st.line_chart(df[['date', 'closing_value']].set_index('date'))

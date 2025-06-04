6a89f8b007d7338d3c0eb0f14a0c3035c3e26e87
heitor <heitor.nolla@gmail.com>
2025-06-04 10:58:43 -0300

    request working

--- src/extract_load.py
+++ src/extract_load.py
@@ -5,7 +5,23 @@ import pandas as pd
 from sqlalchemy import create_engine
 import yfinance as yf
 
-commodities = ['CL=F', 'GC=F', 'SI=F']
 
-def buscas_dados(ticker, periodo='5d', intervalo='1d'):
+def get_commodity_data(ticker, period='5d', interval='1d'):
   ticker = yf.Ticker(ticker)
+  data = ticker.history(period=period, interval=interval)[['Close']]
+  data['symbol'] = ticker
+  return data
+
+
+def get_all_commodity_data(commodities):
+  data = []
+  for symbol in commodities:
+    data.append(get_commodity_data(symbol))
+
+  return pd.concat(data)
+
+
+if __name__ == "__main__":
+  commodities = ['CL=F', 'GC=F', 'SI=F']
+  data = get_all_commodity_data(commodities)
+  print(data)
